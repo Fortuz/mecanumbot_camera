eb4d04bde9b502d032d7e96b4bb0f021eec9049d
RolandSandorNagy <roland.s.nagy@gmail.com>
2025-12-06 11:30:55 +0100

    pi camera colour improvement

--- mecanumbot_camera/camera_picam_reader.py
+++ mecanumbot_camera/camera_picam_reader.py
@@ -4,18 +4,24 @@ import cv2
 class PiCamReader:
     def __init__(self, width=640, height=480, fps=30):
         self.picam2 = Picamera2()
+
         config = self.picam2.create_video_configuration(
-            main={"size": (width, height), "format": "RGB888"},
-            controls={"FrameDurationLimits": (1000000//fps, 1000000//fps)}
+            main={"size": (width, height), "format": "BGR888"},
+            controls={
+                "FrameDurationLimits": (1000000//fps, 1000000//fps),
+                "AwbEnable": True,
+            }
         )
+
         self.picam2.configure(config)
         self.picam2.start()
 
     def read(self):
         try:
-            frame = self.picam2.capture_array()
-            return cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-        except:
+            # BGR888 → OpenCV-nek tökéletes, nem kell átkonvertálni
+            return self.picam2.capture_array("main")
+        except Exception as e:
+            print("READ ERROR:", e)
             return None
 
     def stop(self):
